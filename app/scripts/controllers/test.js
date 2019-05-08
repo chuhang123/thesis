bb0b0b58d2c7a77b5325982bd90ad0ccd5f871c4
chuhang123 <chuhang@yunzhiclub.com>
2019-05-08 15:03:30 +0800

    teste

---
+++ app/scripts/controllers/test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thesisApp.controller:TestCtrl
+ * @description
+ * # TestCtrl
+ * Controller of the thesisApp
+ */
+angular.module('thesisApp')
+  .controller('TestCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
